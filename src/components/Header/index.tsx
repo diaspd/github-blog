69c2697b8ebae62e8808116f53c02be3faa2a368
Pedro Dias <pdr050203@gmail.com>
2024-02-11 20:43:28 -0300

    fix: logo img

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import { HeaderContainer } from './styles'
 
+import Logo from '../../assets/logo.svg'
+
 export function Header() {
   return (
     <HeaderContainer>
       <div>
-        <img src="../src/assets/logo.svg" alt="logo" />
+        <img src={Logo} alt="logo" />
         <h1>GITHUB BLOG</h1>
       </div>
     </HeaderContainer>
