eda1b914469e47aec8bbead08d5c465350a306f7
Pedro Dias <pdr050203@gmail.com>
2024-02-11 20:53:26 -0300

    rmv: logo img type

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,10 @@
 import { HeaderContainer } from './styles'
 
-import Logo from '../../../public/logo.svg'
-
 export function Header() {
   return (
     <HeaderContainer>
       <div>
-        <img src={Logo} alt="logo" />
+        <img src="../../../public/logo.svg" alt="logo" />
         <h1>GITHUB BLOG</h1>
       </div>
     </HeaderContainer>
